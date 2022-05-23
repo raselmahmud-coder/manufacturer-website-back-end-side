ca5a1c980c6eb3728983ac37872cc7d81543a647
RASEL MAHMUD <raselmahmud98262@gmail.com>
2022-05-23 19:23:07 +0800

    tools api added

--- index.js
+++ index.js
@@ -13,7 +13,33 @@ app.use(express.json());
 app.get("/", (req, res) => {
   res.send("assignment 12 running the server");
 });
-
 app.listen(port, () => {
   console.log(`assignment 12 app listening on port ${port}`);
 });
+// db uri
+// import { MongoClient } from "mongodb";
+const { MongoClient, ServerApiVersion } = require('mongodb');
+const uri = `${process.env.URI}`;
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+
+// db connection
+async function run() {
+  try {
+    await client.connect();
+    const toolsCollection = client.db("autoparts-assign-12").collection("Tools");
+    console.log("toolsCollection is ready");
+    // for tools 
+    app.get('/tools', async (req, res) => {
+      const query = {};
+      const result = await toolsCollection.find(query).toArray();
+      // console.log("tools",result);
+      res.send(result)
+      
+    })
+
+  } catch (err) {
+    console.log("error getting",err);
+  }
+}
+run().catch(console.dir);
+
