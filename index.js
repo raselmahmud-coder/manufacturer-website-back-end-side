3934790e33321f671b09444c6b4f8203a5f89488
RASEL MAHMUD <raselmahmud98262@gmail.com>
2022-05-24 04:04:53 +0800

    get a specific tool API added

--- index.js
+++ index.js
@@ -18,28 +18,42 @@ app.listen(port, () => {
 });
 // db uri
 // import { MongoClient } from "mongodb";
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 const uri = `${process.env.URI}`;
-const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+const client = new MongoClient(uri, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  serverApi: ServerApiVersion.v1,
+});
 
 // db connection
 async function run() {
   try {
     await client.connect();
-    const toolsCollection = client.db("autoparts-assign-12").collection("Tools");
+    const toolsCollection = client
+      .db("autoparts-assign-12")
+      .collection("Tools");
     console.log("toolsCollection is ready");
-    // for tools 
-    app.get('/tools', async (req, res) => {
+    // for tools
+    app.get("/tools", async (req, res) => {
       const query = {};
       const result = await toolsCollection.find(query).toArray();
-      // console.log("tools",result);
-      res.send(result)
+      res.send(result);
+    });
+    // get a specific tool
+    app.get("/tool/:id", async (req, res) => {
+      const { id } = req.params;
+      const query = { _id: ObjectId(id) };
+      const result = await toolsCollection.findOne(query);
+      // console.log("got result", result);
+      res.send(result);
+    });
+    // users update in data base with set a role
+    app.put("/users/:email", async (req, res) => {
       
     })
-
   } catch (err) {
-    console.log("error getting",err);
+    console.log("error getting", err);
   }
 }
 run().catch(console.dir);
-
